0e5b3decc2a4705ec881fce5d5dca118ca9729ed
Shaun Lobsinger <shaun.lobsinger@gmail.com>
2020-10-14 23:28:03 -0700

    added immer to the mix

--- src/createReducer.js
+++ src/createReducer.js
@@ -1,4 +1,8 @@
+/* eslint-disable default-case */
+/* eslint-disable no-case-declarations */
+/* eslint-disable no-param-reassign */
 import { snakeCase } from 'snake-case';
+import produce from 'immer';
 import moment from 'moment';
 
 import { createActions, createActionCreators } from './utils/actions';
@@ -24,48 +28,51 @@ export const configureReducers = (rootConfig) => (reducerConfig) => {
   const actionCreators = createActionCreators(actions, config);
   const thunks = createThunks(actionCreators);
 
-  const reducer = (state = INITIAL_STATE, action) => {
+  const reducer = produce((draft, action) => {
     switch (action.type) {
       case actions.loadCollection:
-        return {
-          ...state,
-          records: action.data,
-          lastModified: moment(),
-          loaded: action.data.map((item) => `${item.id}`),
-        };
+        draft.records.concat(action.data);
+        draft.lastModified = moment();
+        draft.loaded.concat(action.data.map((item) => `${item.id}`)).filter((item, index, array) => array.indexOf(item) === index);
+
+        break;
       case actions.loadSingle:
-      case actions.update:
       case actions.create:
-        return {
-          ...state,
-          records: [
-            ...state.records.filter((record) => record.id !== action.data.id),
-            action.data,
-          ],
-          lastModified: moment(),
-        };
+        draft.records.push(action.data);
+        draft.lastModified = moment();
+        draft.loaded.push(action.data.id);
+        draft.loaded.filter((item, index, array) => array.indexOf(item) === index);
+
+        break;
+      case actions.update:
+        const index = draft.records.findIndex((record) => record.id === action.data.id);
+
+        if (index) {
+          draft.records.splice(index, 1, action.data);
+          draft.lastModified = moment();
+        }
+
+        break;
       case actions.loading:
-        return {
-          ...state,
-          loading: true,
-        };
+        draft.loading = true;
+
+        break;
       case actions.delete:
-        return {
-          ...state,
-          records: [
-            ...state.records.filter((record) => record.id !== action.data.id),
-          ],
-          lastModified: moment(),
-        };
+        const removeIndex = draft.records.findIndex((record) => record.id === action.data.id);
+
+        if (index) {
+          draft.splice(removeIndex, 1);
+          draft.lastModified = moment();
+          draft.loaded.filter((item) => item !== action.data.id);
+        }
+
+        break;
       case actions.error:
-        return {
-          ...state,
-          error: action.data,
-        };
-      default:
-        return state;
+        draft.error = action.data;
+
+        break;
     }
-  };
+  }, INITIAL_STATE);
 
   return {
     reducer,
