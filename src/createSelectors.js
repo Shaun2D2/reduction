4d5842e7db249849dd6593d51e457611e4f52892
Shaun Lobsinger <shaun@jolobo.com>
2020-10-25 22:15:54 -0700

    added additional selectors

--- src/createSelectors.js
+++ src/createSelectors.js
@@ -1,28 +1,43 @@
+import { create } from 'lodash';
 import { createSelector } from 'reselect';
 
 const createSelectors = ({ resource }) => {
-  const records = (state) => state[resource].collection;
+  const resourceState = (state) => state[resource];
+
+  const getCollection = createSelector(
+    resourceState,
+    (state) => state.collection,
+  );
 
   const getSingle = createSelector(
-    records,
+    getCollection,
     (collection) => collection[0],
   );
 
   const getSingleById = createSelector(
-    records,
+    getCollection,
     (id) => id,
     (collection, id) => collection.find((record) => record.id === id),
   );
 
-  const getCollection = createSelector(
-    records,
-    (collection) => collection,
+  const getLoadedIds = createSelector(
+    resourceState,
+    (state) => state.loaded,
+  );
+
+  const getRecordByPropertyValue = createSelector(
+    getCollection,
+    (property) => property,
+    (value) => value,
+    (collection, property, value) => collection.find((item) => item[property] === value),
   );
 
   return {
     getSingle,
     getSingleById,
     getCollection,
+    getLoadedIds,
+    getRecordByPropertyValue,
   };
 };
 
